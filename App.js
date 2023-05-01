251f4dc2e9506095909594f4615c4431a63881df
AJAX <22mapbs019@ddu.ac.in>
2023-05-01 14:19:16 +0530

    APK development Done

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Nav from './Router/Nav';
 import { Setting } from './Redux/store';
 import { StatusBar } from 'react-native';
+import SplashScreen from 'react-native-splash-screen'
 export default function App() {
   /*Hoocks*/
   const [settings, setSetting] = useState(Setting);
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <NavigationContainer>
       <StatusBar backgroundColor={'#2196F3'} />
